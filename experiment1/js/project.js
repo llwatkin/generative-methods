9042027f202d132bc757cd6f3309fb39f19b68d4
Lyle Watkins <llwatkin@ucsc.edu>
2025-04-04 20:41:18 -0700

    Modified home page and added Glitch project

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -1,34 +1,56 @@
-// project.js - purpose and description here
-// Author: Your Name
-// Date:
+// project.js - A random muffin recipe generator
+// Author: Lyle Watkins
+// Date: 4 April 2025
 
-// NOTE: This is how we might start a basic JavaaScript OOP project
+const fillers = {
+  adj: ['Yummy', 'Delicious', 'Unique', 'Unusual', 'Tasty', 'Easy', 'Simple', 'Strange', 'Human', 'Cool', 'Creative', 'Nostalgic', 'Savory', 'Sweet'],
+  flour: ['all-purpose', 'almond', 'coconut', 'oat', 'hemp', 'gluten-free', 'rice', 'soy'],
+  sugar: ['light brown sugar', 'brown sugar', 'granulated sugar', 'powdered sugar', 'pearl sugar', 'cane sugar', 'coconut sugar', 'honey', 'agave nectar', 'maple syrup', 'molasses'],
+  oil: ['vegetable oil', 'safflower oil', 'avocado oil', 'butter', 'olive oil', 'peanut oil', 'lard', 'coconut oil', 'almond oil', 'macadamia oil'],
+  milk: ['chocolate', 'whole', 'strawberry', 'coconut', 'peanut', 'oat', 'soy', 'skim', 'goat', 'sheep', 'buffalo', 'almond'],
+  seasoning: ['pepper', 'cinnamon', 'nutmeg', 'oregano', 'allspice', 'basil', 'paprika', 'cumin', 'ginger', 'cacao powder'],
+  extract: ['vanilla', 'lemon', 'orange', 'lime', 'almond', 'peppermint', 'coconut', 'banana', 'maple', 'rum'],
+  special: ['blueberries', 'chopped strawberries', 'peanuts', 'chopped almonds', 'chopped walnuts', 'macadamia nuts', 'chocolate chips', 'chocolate chunks', 'coconut flakes', 'shredded chicken', 'shredded beef', 'chopped carrots', 'chopped broccoli', 'chopped banana', 'beans', 'chopped mango', 'chopped pear', 'chopped apple'],
+};
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
+const template = `$adj Muffin Recipe
 
-// define a class
-class MyProjectClass {
-  // constructor function
-  constructor(param1, param2) {
-    // set properties using 'this' keyword
-    this.property1 = param1;
-    this.property2 = param2;
-  }
-  
-  // define a method
-  myMethod() {
-    // code to run when method is called
+1 1/2 cups $flour flour
+3/4 cup $sugar
+2 tsp baking powder
+1/4 tsp salt
+1/2 tsp $seasoning
+1/3 cup $oil
+1 large egg
+1/3 cup $milk milk
+1 tsp $extract extract
+1 cup $special`;
+
+
+// STUDENTS: You don't need to edit code below this line.
+
+const slotPattern = /\$(\w+)/;
+
+function replacer(match, name) {
+  let options = fillers[name];
+  if (options) {
+    return options[Math.floor(Math.random() * options.length)];
+  } else {
+    return `<UNKNOWN:${name}>`;
   }
 }
 
-function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
+function generate() {
+  let story = template;
+  while (story.match(slotPattern)) {
+    story = story.replace(slotPattern, replacer);
+  }
 
-  // call a method on the instance
-  myInstance.myMethod();
+  /* global box */
+  box.innerText = story;
 }
 
-// let's get this party started - uncomment me
-//main();
+/* global clicker */
+clicker.onclick = generate;
+
+generate();
